44d9e8c40c0fed378d99b2e78ac7a83af6a08939
CalmTurkey <Raymond_taumata@hotmail.com>
2021-01-19 13:32:00 +1300

    Line 3 Added.

--- Part D/src/Part_D.java
+++ Part D/src/Part_D.java
@@ -4,6 +4,7 @@ public class Part_D {
 	public static void main(String[] args) {
 		System.out.println("Hi, my name is Raymond");
 		System.out.println("My favourite colour is blue");
+		System.out.println("My favourite mode of transportation is space ship");
 		
 	}
 
